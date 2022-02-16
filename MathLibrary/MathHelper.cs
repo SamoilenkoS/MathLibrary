e8e7b6eb4357f7492a6b7563ff48804db9641437
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-02-16 19:15:57 +0200

    Add project files.

---
+++ MathLibrary/MathHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class MathHelper
+    {
+        public static int Max(int a, int b, int c)
+        {
+            return a > b ? (a > c ? a : c) : (b > c ? b : c);
+        }
+    }
+}
