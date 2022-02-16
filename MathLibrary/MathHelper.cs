990cdc4612148b352c9d7bbb0f0318e2f61c16f6
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-02-16 19:50:49 +0200

    Added UT for MathHelper methods

--- MathLibrary/MathHelper.cs
+++ MathLibrary/MathHelper.cs
@@ -8,5 +8,24 @@ namespace MathLibrary
         {
             return a > b ? (a > c ? a : c) : (b > c ? b : c);
         }
+
+        public static int Max(int[] array)
+        {
+            if (array != null && array.Length > 0)
+            {
+                int maxValue = array[0];
+                for (int i = 1; i < array.Length; i++)
+                {
+                    if(maxValue < array[i])
+                    {
+                        maxValue = array[i];
+                    }
+                }
+
+                return maxValue;
+            }
+
+            throw new ArgumentException();
+        }
     }
 }
